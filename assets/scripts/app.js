753107afe405f8add2a6f637c389d6cb2d184aca
hussainHakimi <hussainbayan614@gmail.com>
2022-04-09 13:44:32 +0430

    Show log part is added.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,11 @@ const HEAL_VALUE = 20;
 
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK'; 
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
 const enteredValue = prompt('Maximum life for you and monster.', '100');
 
@@ -15,8 +20,78 @@ if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
 let currentMosterHealth = chosenMaxLife;
 let currentPlaterHealth = chosenMaxLife;
 let hasBonusLife = true;
+let battleLog = [];
 
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(eve, val, monsterHealth, playerHealth){
+    let logEntry = {
+            event : eve,
+            value : val,
+            finalMonsterHealth : monsterHealth,
+            finalPlayerHealth : playerHealth
+    };
+    // if(eve === LOG_EVENT_PLAYER_ATTACK){
+    //     logEntry.target = 'MONSTER';
+    // } else if(eve === LOG_EVENT_PLAYER_STRONG_ATTACK){
+    //     logEntry = {
+    //         event : eve,
+    //         value : val,
+    //         target : 'MONSTER',
+    //         finalMonsterHealth : monsterHealth,
+    //         finalPlayerHealth : playerHealth
+    //     };
+    // }else if(eve === LOG_EVENT_MONSTER_ATTACK){
+    //     logEntry = {
+    //         event : eve,
+    //         value : val,
+    //         target : 'PLAYER',
+    //         finalMonsterHealth : monsterHealth,
+    //         finalPlayerHealth : playerHealth
+    //     };
+    // }else if(eve === LOG_EVENT_PLAYER_HEAL){
+    //     logEntry = {
+    //         event : eve,
+    //         value : val,
+    //         target : 'PLAYER',
+    //         finalMonsterHealth : monsterHealth,
+    //         finalPlayerHealth : playerHealth
+    //     };
+    // }else if(eve === LOG_EVENT_GAME_OVER){
+    //     logEntry = {
+    //         event : eve,
+    //         value : val,
+    //         finalMonsterHealth : monsterHealth,
+    //         finalPlayerHealth : playerHealth
+    //     };
+    // }
+
+    switch(eve){
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                        event : eve,
+                        value : val,
+                        finalMonsterHealth : monsterHealth,
+                        finalPlayerHealth : playerHealth
+                    };
+            break;
+        default:
+            logEntry = {};
+    }
+    battleLog.push(logEntry);
+}
 
 function reset(){
     currentMosterHealth = chosenMaxLife;
@@ -29,6 +104,9 @@ function endRound(){
     const initialPlayerHealth = currentPlaterHealth;
     const playerDamage =  dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlaterHealth -= playerDamage;
+
+    writeToLog(LOG_EVENT_MONSTER_ATTACK, playerDamage, currentMosterHealth, currentPlaterHealth);
+
     if(currentPlaterHealth <= 0 && hasBonusLife){
         hasBonusLife = false;
         removeBonusLife();
@@ -37,11 +115,14 @@ function endRound(){
         alert('You almost dead but the bonus life saved you');
     }
     if(currentMosterHealth <= 0 && currentPlaterHealth > 0){
-        alert('Congratulate! You Win')
+        alert('Congratulate! You Win');
+        writeToLog(LOG_EVENT_GAME_OVER, 'PLAYER WON', currentMosterHealth, currentPlaterHealth);
     }else if(currentPlaterHealth <= 0 && currentMosterHealth > 0){
         alert('Sorry! You lose the game.');
+        writeToLog(LOG_EVENT_GAME_OVER, 'MONSTER WON', currentMosterHealth, currentPlaterHealth);
     }else if(currentMosterHealth <= 0 && currentPlaterHealth <= 0){
         alert('you have a drow');
+        writeToLog(LOG_EVENT_GAME_OVER, 'A DRAW', currentMosterHealth, currentPlaterHealth);
     }
 
     if(currentMosterHealth < 0 || currentPlaterHealth < 0){
@@ -51,14 +132,21 @@ function endRound(){
 
 
 function attackMonster(mode){
-    let maxDamage;
-    if(mode === MODE_ATTACK ){
-        maxDamage = ATTACK_VALUE
-    }else if(mode === MODE_STRONG_ATTACK){
-        maxDamage = STRONG_ATTACK_VALUE;
-    }
+    // let maxDamage;
+    // let logEvent;
+    // if(mode === MODE_ATTACK ){
+    //     maxDamage = ATTACK_VALUE;
+    //     logEvent = LOG_EVENT_PLAYER_ATTACK;
+    // }else if(mode === MODE_STRONG_ATTACK){
+    //     maxDamage = STRONG_ATTACK_VALUE;
+    //     logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
+    // }
+    const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
+
     const damage = dealMonsterDamage(maxDamage);
     currentMosterHealth -= damage;
+    writeToLog(logEvent, damage, currentMosterHealth, currentPlaterHealth);
 
     endRound();
 }
@@ -81,9 +169,16 @@ function healPlayerHandler(){
     }
     increasePlayerHealth(healValue);
     currentPlaterHealth += healValue;
+
+    writeToLog(LOG_EVENT_PLAYER_HEAL, HEAL_VALUE, currentMosterHealth, currentPlaterHealth);
     endRound();
+}
+
+function printLogHandler(){
+    console.log(battleLog);
 }
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healPlayerHandler);
+logBtn.addEventListener('click', printLogHandler);
